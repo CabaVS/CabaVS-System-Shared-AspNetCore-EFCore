401cb00346fe3fb32e3ba7b91257482cb64e8a3d
Valerii <valerii.savchenko@icloud.com>
2020-10-12 15:12:58 +0300

    Initial commit; Added RunMigrations extension for the IApplicationBuilder

---
+++ Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CabaVS.Shared.AspNetCore.EFCore.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void RunMigrations<TContext>(this IApplicationBuilder app) where TContext : DbContext
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            var serviceScopeFactory = app.ApplicationServices.GetService<IServiceScopeFactory>();
+
+            using var scope = serviceScopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetService<TContext>();
+            if (context == null)
+            {
+                throw new ApplicationException("Unable to run migrations. Chosen DbContext is not available from Service Provider.");
+            }
+
+            if (!context.Database.CanConnect())
+            {
+                throw new ApplicationException("Unable to run migrations. Database is not available.");
+            }
+
+            context.Database.Migrate();
+        }
+    }
+}
